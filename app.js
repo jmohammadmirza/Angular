c9cc1c9b80aa2ba2589209be8923389e3db8ad2c
jmohammadmirza <mdaslameme@gmail.com>
2014-11-03 19:33:40 +0530

    Final code till fruits

---
+++ app.js
@@ -0,0 +1,6 @@
+// JavaScript Document Javed 11/3/2014
+angular.module("animationApp",['ngAnimate']).controller("FruitsController", function($scope){
+
+	$scope.fruits = ["Apple","Apricot","Avocado","Banana","Breadfruit","Bilberry","Blackberry","Blackcurrant","Blueberry","Boysenberry","Cantaloupe","Currant","Cherry","Cherimoya","Cloudberry","Coconut","Cranberry","Cucumber","Damson","Date","Dragonfruit","Durian"];
+
+});
